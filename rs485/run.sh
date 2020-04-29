150682ec371f8cab5b2d1646d0acaaa5d31d485a
kkj5492 <63589880+kkj5492@users.noreply.github.com>
2020-04-29 20:12:53 +0900

    Update run.sh

--- rs485/run.sh
+++ rs485/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SHARE_DIR=/share/rs485
+SHARE_DIR=/share/kocom
 
 if [ ! -f $SHARE_DIR/rs485.py ]; then
 	mkdir $SHARE_DIR
@@ -8,7 +8,10 @@ if [ ! -f $SHARE_DIR/rs485.py ]; then
 fi
 /makeconf.sh
 
-echo "[Info] RS485 To MQTT Controller"
+echo "[Info] Run Wallpad Controller"
 cd $SHARE_DIR
 python3 $SHARE_DIR/rs485.py
+
+# for dev
+#while true; do echo "still live"; sleep 100; done
 
